6c9a9bdf4064b49286704df5b9dc2b011a56c35f
Puritanic <darktasevski@gmail.com>
2017-12-13 21:32:34 +0100

    Add card styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,19 @@ import React, { Component } from 'react';
 export default class ColumnCard extends Component {
   render() {
     return (
-      <div>
-        <ul>
-          <li>SDD</li>
-          <li>IB</li>
-          <li>AI</li>
-          <li>XSS</li>
-          <li>XML</li>
-          <li>ADP</li>
-        </ul>
+      <div className="card">
+        <div>
+          <h3>Card title</h3>
+          <p>Card body</p>
+          <ul>
+            <li>SDD</li>
+            <li>IB</li>
+            <li>AI</li>
+            <li>XSS</li>
+            <li>XML</li>
+            <li>ADP</li>
+          </ul>
+        </div>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => (
   <div className="board__header">
-    <h1>Trello</h1>
-    <p>Log Out</p>
+    <h1 className="board__header__title">Trello</h1>
+    <p className="board__header__logout">Log Out</p>
   </div>
 );
 
