c2a428dd7d0d50c9937fc0cd3a9700b456656758
Puritanic <darktasevski@gmail.com>
2017-12-15 19:46:06 +0100

    Add toggle state to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import CheckList from './CheckList';
 
 export default class ColumnCard extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      showDetails: false,
+    };
+  }
+
+  onToggleDesc = () => {
+    this.setState((state, props) => ({ showDetails: !this.state.showDetails }));
+  };
+
   render() {
-    return (
-      <div className="card">
-        <div className="card__title">{this.props.title}</div>
+    // Var and conditional for toggling description of the card on and off
+    let cardDetails;
+    if (this.state.showDetails) {
+      cardDetails = (
         <div className="card__details">
           {this.props.description}
           <CheckList cardId={this.props.id} tasks={this.props.tasks} />
         </div>
+      );
+    }
+    return (
+      <div className="card">
+        <div className="card__title" onClick={this.onToggleDesc}>
+          {this.props.title}
+        </div>
+        {cardDetails}
       </div>
     );
   }
