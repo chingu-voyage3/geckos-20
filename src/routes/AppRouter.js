a3c26beaa8e2459b21ec2f6d1ed037c4f9bd5b6e
Puritanic <darktasevski@gmail.com>
2018-01-26 21:56:25 +0100

    Add private and public route handlers

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -6,18 +6,20 @@ import App from '../App';
 import NewCard from '../components/NewCard';
 import EditCard from '../components/EditCard';
 import LoginPage from '../components/LoginPage';
+import NotFoundPage from '../components/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
 export default () => (
   <Router history={history}>
     <Switch>
-      <Route exact path="/" component={LoginPage} />
+      <PublicRoute exact path="/" component={LoginPage} />
       <PrivateRoute path="/dashboard" component={App} />
       <PrivateRoute path="/dashboard/new" component={NewCard} />
       <PrivateRoute path="/dashboard/edit/:card_id" component={EditCard} />
-      <Route component={LoginPage} />
+      <Route component={NotFoundPage} />
     </Switch>
   </Router>
 );
